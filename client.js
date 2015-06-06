bf108f9d7b3ab0e8a6f1d5fda78802d01e9b1761
Tall Street <contact@tallstreet.com>
2015-06-06 17:05:31 +0100

    Remove commented out code

--- client.js
+++ client.js
@@ -1,50 +1,3 @@
-/*
-
-var cats = {};
-
-Leap.loop(function(frame) {
-
-  frame.hands.forEach(function(hand, index) {
-    
-    var cat = ( cats[index] || (cats[index] = new Cat()) );    
-    cat.setTransform(hand.screenPosition(), hand.roll());
-    
-  });
-  
-}).use('screenPosition', {scale: 0.25});
-
-
-var Cat = function() {
-  var cat = this;
-  var img = document.createElement('img');
-  img.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/109794/cat_2.png';
-  img.style.position = 'absolute';
-  img.onload = function () {
-    cat.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
-    document.body.appendChild(img);
-  }
-  
-  cat.setTransform = function(position, rotation) {
-
-    img.style.left = position[0] - img.width  / 2 + 'px';
-    img.style.top  = position[1] - img.height / 2 + 'px';
-
-    img.style.transform = 'scaleX(' + -rotation + ')';
-    console.log(rotation);
-    
-    img.style.webkitTransform = img.style.MozTransform = img.style.msTransform =
-    img.style.OTransform = img.style.transform;
-
-  };
-
-};
-
-cats[0] = new Cat();
-
-// This allows us to move the cat even whilst in an iFrame.
-Leap.loopController.setBackground(true)
-
-*/
 
 var controller = Leap.loop({enableGestures: true}, function(frame){
   if(frame.valid && frame.gestures.length > 0){
@@ -73,9 +26,9 @@ function setup_acc(title, content){
 	$(title).click();
 }
 
-$(function(){ 
+$(function(){
 	$("#video-active").on(
- 	   "timeupdate", 
+ 	   "timeupdate",
     	function(event){
     		onTrackedVideoFrame(this.currentTime, this.duration);
     	}
@@ -131,7 +84,7 @@ function snap() {
 	//console.log(canvas);
 	// Set the canvas width and height to the values just calculated
 	//canvas.width = w;
-	//canvas.height = h;			
+	//canvas.height = h;
 
 	// Get a handle on the 2d context of the canvas element
 	//var context = canvas.element.get(0).getContext('2d');
